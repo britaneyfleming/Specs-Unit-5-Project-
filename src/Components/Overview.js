18503a1d7f9c39b34a0cd67f733f0f1b37f63351
Britaney Fleming <britaneyrfleming@gmail.com>
2023-02-12 19:29:33 -0600

    Updated

--- src/Components/Overview.js
+++ src/Components/Overview.js
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 import {selectDisplay } from "../redux/slices/displayCountrySlice";
 
 const Overview = () => {
-    let currentDisplay = useSelector(selectDisplay):
+    let currentDisplay = useSelector(selectDisplay);
     return (
         <div className="stack">
             <h1>{currentDisplay.name.official}</h1>
